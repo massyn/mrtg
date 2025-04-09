6101ef6279c31366696303764dc91c51a2c95c8b
massyn <phil.massyn@icloud.com>
2025-04-09 00:53:21 +0000

    Update latency

--- latency.sh
+++ latency.sh
@@ -2,8 +2,9 @@
 HOST=%HOST%
 COUNT=10
 DTE=$(date '+%Y-%m-%d %H:%M:%S')
-VAR=$(ping $HOST -c $COUNT | tail -1 | cut -d \/ -f 5)
-echo $VAR
-echo $VAR
+VAR=$(ping $HOST -c $COUNT | tail -1)
+# | cut -d \/ -f 5)
+echo $VAR | cut -d \/ -f 5
+echo $VAR | cut -d \/ -f 6
 uptime -s
 echo $HOST
